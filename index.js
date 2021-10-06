b2368f5fb3e903bb1de6b21adf03d01ed44d4bf5
CGrisham1 <89211813+CGrisham1@users.noreply.github.com>
2021-10-05 21:25:41 -0500

    somewhere on setting up the weather API....

--- index.js
+++ index.js
@@ -1,4 +1,39 @@
 import { Header, Nav, Main, Footer } from "./components";
+import axios from "Axios";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const router = new Navigo(window.location.origin);
+
+router.hooks({
+  before: (done, params) => {
+    const page = params && params.hasOwnProperty("page") ? capitalize(params.page) : "Home";
+
+    if (page === "Home") {
+      axios
+        .get(
+          `https://api.openweathermap.org/data/2.5/weather?appid=fbb30b5d6cf8e164ed522e5082b49064&q=st.%20louis`
+        )
+        .then(response => {
+          state.Home.weather = {};
+          state.Home.weather.city = response.data.name;
+          state.Home.weather.temp = response.data.main.temp;
+          state.Home.weather.feelsLike = response.data.main.feels_like;
+          state.Home.weather.description = response.data.weather[0].main;
+          done();
+        })
+        .catch(err => console.log(err));
+    }
+  }
+});
+
+
+
+
+
+
+
 
 function render() {
   document.querySelector("#root").innerHTML = `

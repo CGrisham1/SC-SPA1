162934be1a5f86c3fcc99a94659a518814797273
CGrisham1 <89211813+CGrisham1@users.noreply.github.com>
2021-10-06 19:02:56 -0500

    more files to root index.js

--- index.js
+++ index.js
@@ -26,7 +26,8 @@ function render(st) {
   router.updatePageLinks();
 
   addEventListeners(st);
-}
+};
+
 
 function addEventListeners(st) {
 
@@ -36,12 +37,77 @@ function addEventListeners(st) {
       render(state[event.target.title]);
     })
   );
-
+};
 
 // add menu toggle to bars icon in nav bar
 document.querySelector(".fa-bars").addEventListener("click", () => {
   document.querySelector("nav > ul").classList.toggle("hidden--mobile");
 });
+
+// event listener for the the photo form
+if (st.view === "Form") {
+  document.querySelector("form").addEventListener("submit", event => {
+    event.preventDefault();
+    // convert HTML elements to Array
+    let inputList = Array.from(event.target.elements);
+    // remove submit button from list
+    inputList.pop();
+    // construct new picture object
+    let newPic = inputList.reduce((pictureObject, input) => {
+      pictureObject[input.name] = input.value;
+      return pictureObject;
+    }, {});
+    // add new picture to state.Gallery.pictures
+    state.Gallery.pictures.push(newPic);
+    render(state.Gallery);
+  });
+};
+
+// 5. Router.hooks
+router.hooks({
+  before: (done, params) => {
+    const page =
+      params && params.hasOwnProperty("page")
+        ? capitalize(params.page)
+        : "Home";
+    if (page === "Home") {
+      axios
+        .get(
+          `https://api.openweathermap.org/data/2.5/weather?appid=${process.env.OPEN_WEATHER_MAP_API_KEY}&q=st.%20louis`
+        )
+        .then(response => {
+          state.Home.weather = {};
+          state.Home.weather.city = response.data.name;
+          state.Home.weather.temp = response.data.main.temp;
+          state.Home.weather.feelsLike = response.data.main.feels_like;
+          state.Home.weather.description = response.data.weather[0].main;
+          done();
+        })
+        .catch(err => console.log(err));
+    }
+    if (page === "Pizza") {
+      axios
+        .get(`${process.env.PIZZA_PLACE_API_URL}`)
+        .then(response => {
+          state.Pizza.pizzas = response.data;
+          done();
+        })
+        .catch(error => {
+          console.log("It puked", error);
+        });
+    }
+  }
+});
+
+
+
+
+
+
+
+
+
+
 
 // array of pictures for gallery
 // const dogPictures = [
